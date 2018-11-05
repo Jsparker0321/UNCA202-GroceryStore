4a30f8445be236ff1e75dbbf392943978c24aa52
KH037s <KH037s@KH037-02.uncanet.unca.edu>
2018-11-05 13:28:15 -0500

    Needs code implementation, most methods have been written

--- GroceryStore/src/Customer.java
+++ GroceryStore/src/Customer.java
@@ -11,4 +11,25 @@
  */
 public class Customer {
 	
+	private int groceryItems;
+	private int timeRemaining;
+	private int processCart;
+	
+	public Customer( int groceryItems, int processCart){
+		this.groceryItems = groceryItems;
+		this.processCart = processCart;
+	}
+	
+	public int getgroceryItems() {
+		return groceryItems;
+	}
+	
+	public int getprocessCart() {
+		return processCart;
+	}
+	
+	public void decreaseTime() {
+		
+	}
+	 
 }
--- GroceryStore/src/GroceryStore.java
+++ GroceryStore/src/GroceryStore.java
@@ -8,6 +8,38 @@
  * @version A Grocery Store
  * @version CSCI 202
  */
+
+import java.util.*;
+import java.util.ArrayList;
+
 public class GroceryStore {
 
+	ArrayList<Queue> lineOne = new ArrayList<Queue>();
+	ArrayList<Queue> lineTwo = new ArrayList<Queue>();
+	ArrayList<Queue> lineThree = new ArrayList<Queue>();
+	ArrayList<Queue> lineFour = new ArrayList<Queue>();
+	private int lines;
+
+	public GroceryStore(int lines) {
+		this.lines = lines;
+	}
+
+	public void run(/** @param, @param, @param @param **/
+	) {
+
+	}
+
+	public void printData() {
+		/**
+		 * print: 
+		 * (a) the number of time steps run 
+		 * (b) the number of lines 
+		 * (c) the customer arrival probability 
+		 * (d) the time per item 
+		 * (e) the maximum number of items 
+		 * (f) the maximum length of each queue 
+		 * (g) the number of customers left in each queue 
+		 * (h) the total number of customers served
+		 */
+	}
 }
