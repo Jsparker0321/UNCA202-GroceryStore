0d249d222570b36f1f64e9fbf2a7e1aec976da58
jessp <jessp@10.120.165.214>
2018-11-19 12:31:18 -0500

    Updated commit

--- GroceryStore/src/GroceryStore.java
+++ GroceryStore/src/GroceryStore.java
@@ -20,10 +20,10 @@ public class GroceryStore {
 
 	ArrayList<Queue> line;
 	Random gen = new Random();	
-	private int maxItems = gen.nextInt(80) + 1; //A customer will not have more than 80 items
-	private int processCart = 3; //Fixed time it takes to scan and bag a single item
+	private int maxItems; //A customer will not have more than 80 items
+	private int processCart; //Fixed time it takes to scan and bag a single item
 	private int shiftDuration;
-	private double arrivalProbability = .50;
+	private double arrivalProbability;
 	
 
 	public GroceryStore(int lines) {
@@ -40,32 +40,59 @@ public class GroceryStore {
 		
 		
 	}
+	
+	public Queue getShortestQueue() {
+		
+		Queue shortestQueue;
+		int start = 0;
+		
+		for (int i = 1; i < line.size(); i++)
+			
+			if( line.get(start).size() >  line.get(i).size()  )
+				start = i;
+		
+		shortestQueue = line.get(start);
+		
+		return shortestQueue;
+	}
 
 	public void run( int shiftDuration, double arrivalProbability, int processCart, int maxItems) {
 		
+		this.shiftDuration = shiftDuration;
+		this.maxItems = maxItems; //A customer will not have more than 80 items
+		this.processCart = processCart; //Fixed time it takes to scan and bag a single item
+		this.arrivalProbability = arrivalProbability;
+		Customer one = new Customer(gen.nextInt(maxItems)+1, processCart);
 		
+		Queue shortestQueue;
 		
 		for (int time=0; time < shiftDuration; time++) {
 			if (gen.nextDouble() <= arrivalProbability) {
 				shortestQueue = getShortestQueue(); 
-				shortestQueue.enqueue(Customer);
+				shortestQueue.add(one);
 			}
 		}
-
+		
+		
+		//remove customer
+		if (maxItems == 0) {
+			shortestQueue.remove();
+		}
+		//print
+		//reset
 
 	}
 
 	public void printData() {
-		/**
-		 * print: 
-		 * (a) the number of time steps run 
-		 * (b) the number of lines 
-		 * (c) the customer arrival probability 
-		 * (d) the time per item 
-		 * (e) the maximum number of items 
-		 * (f) the maximum length of each queue 
-		 * (g) the number of customers left in each queue 
-		 * (h) the total number of customers served
-		 */
+		  
+		 System.out.println("Duration of shift: " + shiftDuration); // (a) the number of time steps run: shiftduration
+		 System.out.println("Checkout lines available: " + line );  // (b) the number of lines: lines
+		 System.out.println("Probability of a customer arriving: " + arrivalProbability); //(c) the customer arrival probability: arrivalProbability
+		 System.out.println("Time it takes to scan and bag an item: " + processCart); //(d) the time per item: processCart
+		 System.out.println("Number of items in cart: " + maxItems); //(e) the maximum number of items: maxItems 
+		// System.out.println("There are ); //(f) the maximum length of each queue: get length of queue: need a new array declare as global and use for loop
+		 //(g) the number of customers left in each queue: get size of queue 
+		 //(h) the total number of customers served: new variable and increment
+		 
 	}
 }
